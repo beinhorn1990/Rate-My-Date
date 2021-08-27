d13f6aabcb662a0948de5ce7ad3682bcb65c3d84
Brendan Einhorn <beinhorn90@gmail.com>
2021-08-26 20:09:59 -0400

    commit with first migration

--- Models/DatabaseContext.cs
+++ Models/DatabaseContext.cs
@@ -14,7 +14,7 @@ namespace RateMyDate.Models
         private static bool LOG_SQL_STATEMENTS_IN_DEVELOPMENT = false;
 
         // Add database tables here
-
+        public DbSet<DateLocation> DateLocations { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
---
+++ Models/exampledata.sql
@@ -0,0 +1,6 @@
+TRUNCATE TABLE "DateLocations" RESTART IDENTITY;
+
+INSERT INTO "DateLocations" ("Name", "Description", "Address", "Telephone") VALUES ('Thoughtbeat', 'Inverse zero administration benchmark', '07 Meadow Vale Drive', '314-651-9791');
+INSERT INTO "DateLocations" ("Name", "Description", "Address", "Telephone") VALUES ('Dabtype', 'Organized stable firmware', '7 Miller Park', '523-760-6681');
+INSERT INTO "DateLocations" ("Name", "Description", "Address", "Telephone") VALUES ('Topdrive', 'Object-based interactive application', '65 Eliot Lane', '650-993-7074');
+INSERT INTO "DateLocations" ("Name", "Description", "Address", "Telephone") VALUES ('Avaveo', 'Persistent zero defect process improvement', '2 Clarendon Junction', '715-663-5265');
